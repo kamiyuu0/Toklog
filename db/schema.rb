34489a926312e4eeb56d144c24b44134ed9a40c5
yuekami <yuekami7@gmail.com>
2025-06-28 00:04:51 +0900

    fix: Usersテーブルの修正

--- db/schema.rb
+++ db/schema.rb
@@ -22,7 +22,7 @@ ActiveRecord::Schema[7.2].define(version: 2025_06_27_145419) do
   end
 
   create_table "users", force: :cascade do |t|
-    t.string "name", null: false
+    t.string "name"
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
     t.string "reset_password_token"
