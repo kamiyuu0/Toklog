399349575316b541dae4ceb8a02f4c2da825177e
yuekami <yuekami7@gmail.com>
2025-06-28 00:05:30 +0900

    feat: devise導入完了

--- app/controllers/users/sessions_controller.rb
+++ app/controllers/users/sessions_controller.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
 class Users::SessionsController < Devise::SessionsController
-  # before_action :configure_sign_in_params, only: [:create]
+  before_action :configure_sign_in_params, only: [:create]
 
   # GET /resource/sign_in
   # def new
@@ -21,7 +21,17 @@ class Users::SessionsController < Devise::SessionsController
   # protected
 
   # If you have extra params to permit, append them to the sanitizer.
-  # def configure_sign_in_params
-  #   devise_parameter_sanitizer.permit(:sign_in, keys: [:attribute])
-  # end
+  def configure_sign_in_params
+    devise_parameter_sanitizer.permit(:sign_in, keys: [:name])
+  end
+
+  def after_sign_in_path_for(resource)
+    # サインイン後にリダイレクトさせたいパスを記載
+    root_path
+  end
+
+  def after_sign_out_path_for(resorce)
+    # サインアウト後にリダイレクトさせたいパスを記載
+    new_user_session
+  end
 end
